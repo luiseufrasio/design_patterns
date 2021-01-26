cf9c04406d454b2d02e3e8f023531df622df4d90
Luis <luis.eufrasio@gmail.com>
2021-01-26 14:00:06 -0300

    decorator improved

--- src/structural/decorator/Main.java
+++ src/structural/decorator/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		var input = String.format("Name\t\tSalary\n%s\t%d\n%s\t%d\n", 
-			"John Smith", 100000, "Steven Jobs", 912000);
-		var filePath = "../bin/OutputFile.txt";
+			"John Smith", 100000, "Steve Jobs", 912000);
+		var filePath = "OutputFile.txt";
 		
 		DataSourceDecorator encodedDataSource = 
 			new CompressionDecorator(
@@ -15,14 +15,14 @@ public class Main {
 		encodedDataSource.writeData(input);
 		DataSource plainDataSource = new FileDataSource(filePath);
 		
-		System.out.println("- Input ----------------");
+		System.out.println("- Input --------------");
 		System.out.println(input);
 		
-		System.out.println("- Encoded ----------------");
+		System.out.println("- Encoded --------------------------------------------------------------------------------------");
 		System.out.println(plainDataSource.readData());
 		System.out.println();
 		
-		System.out.println("- Decoded ----------------");
+		System.out.println("- Decoded ------------");
 		System.out.println(encodedDataSource.readData());
 	}
 
